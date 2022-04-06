633d3f46ffd8c7939edea5d4ccc968fb177565a1
user <maxter567terry@gmail.com>
2022-04-06 08:58:51 +0300

    Добавьте файлы проекта.

---
+++ TECHSHOP/ORDERZ.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Resources;
+using System.Windows.Forms;
+
+namespace TECHSHOP
+{
+    public partial class ORDERZ : Form
+    {
+        public ORDERZ()
+        {
+            InitializeComponent();
+        }
+
+        private void ORDERZ_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
